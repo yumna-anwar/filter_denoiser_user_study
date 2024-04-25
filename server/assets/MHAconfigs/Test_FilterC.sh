fa4b02b6661832d7956d3b11ddcf1f35243643ba
yumna-anwar <yumnaanwar95@gmail.com>
2024-04-25 14:46:19 -0500

    Relative paths changed

--- server/assets/MHAconfigs/Test_FilterC.sh
+++ server/assets/MHAconfigs/Test_FilterC.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-export MHA_LIBRARY_PATH=/Users/yumnaanwar/Desktop/Audio_Denoiser/openMHA-master/lib
-export PATH=/Users/yumnaanwar/Desktop/Audio_Denoiser/openMHA-master/bin:$PATH
-source /Users/yumnaanwar/Desktop/Audio_Denoiser/openMHA-master/bin/thismha.sh
+export MHA_LIBRARY_PATH=../../../openMHA-master/lib
+export PATH=../../../openMHA-master/bin:$PATH
+source ../../../openMHA-master/bin/thismha.sh
 
 
 echo "Source file: $1"
 echo "dest file: $2"
 echo "Gain Table: $3"
 
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
 FILTERA_CFG="$SCRIPT_DIR/FilterC.cfg"
 
 
